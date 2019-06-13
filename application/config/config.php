d5ad943329064b5b1eed43052f660b829c8fd339
Seyikins27 <seyiboyvvu@gmail.com>
2019-06-13 12:09:06 +0100

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/classbox';
+$config['base_url'] = 'https://classbox.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
